73c23dbd6fc9c45ee069e43d3baa26c1906049c8
DarioVergaraOrtiz <167145189+DarioVergaraOrtiz@users.noreply.github.com>
2024-05-06 18:48:56 -0500

    Proyecto 003 comprometido

---
+++ Proyecto003/src/Main/Principal.java
@@ -0,0 +1,46 @@
+package Main;
+
+import java.sql.Connection;
+import java.util.List;
+
+import Modelo.Alumno;
+import Modelo.AlumnoDAO;
+
+/**
+ * @author Dario Vergara
+ * Tittle: Conexion Base de datos 
+ */
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Connection conn = Conexion.Database.connect();
+        if (conn != null) {
+        	
+            System.out.println("Conexión establecida.");
+            
+        } else {
+        	
+            System.out.println("No se pudo establecer la conexión.");
+        }
+        
+        
+        AlumnoDAO alumnoDAO = new AlumnoDAO(conn);
+        
+        
+        
+        
+        Alumno eliminarAlumno = alumnoDAO.get(1);
+        
+        alumnoDAO.delete(eliminarAlumno);
+
+       
+        List<Alumno> alumnos = alumnoDAO.getAll();
+        for (Alumno alumno : alumnos) {
+            System.out.println(alumno.getId() + " " + alumno.getName() + " " + alumno.getLastname() + ", " + alumno.getAge());
+        }
+
+
+	}
+
+}
